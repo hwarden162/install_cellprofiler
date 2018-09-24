c15792537c242b40083e8c0092ddc7a6f6b5782a
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-09-24 14:14:34 +0100

    update cellprofiler install

--- install_new_cellprofiler.sh
+++ install_new_cellprofiler.sh
@@ -32,7 +32,7 @@ set -e
 # exit the install script if anything fails
 
 # load anaconda
-module load anaconda/5.0.1
+module load anaconda
 
 ENV_NAME=cellprofiler
 GROUP_NAME=igmm_datastore_Drug-Discovery
@@ -41,9 +41,9 @@ DRUG_DISCOVERY_PREFIX=/exports/igmm/eddie/Drug-Discovery
 # check user is in the Drug-Discovery group
 if groups "$USER" | grep -qw "$GROUP_NAME"
 then
-    # set up conda env to save packages in Drug-Discovery, not the in the home
-    # directory -- otherwise we run out of space
-    mkdir "$DRUG_DISCOVERY_PREFIX"/"$USER"
+    if [ ! -d "DRUG_DISCOVERY_PREFIX"/"$USER" ]; then
+        mkdir "$DRUG_DISCOVERY_PREFIX"/"$USER"
+    fi
     CONDA_PKGS=""$DRUG_DISCOVERY_PREFIX"/"$USER"/.conda_pkgs"
     CONDA_ENVS=""$DRUG_DISCOVERY_PREFIX"/"$USER"/.conda_envs"
 else
@@ -51,9 +51,18 @@ else
     exit 1
 fi
 
-# create a .condarc file in your home directory
-# create directories for conda
 
+
+# set up conda env to save packages in Drug-Discovery, not the in the home
+# directory -- otherwise we run out of space
+
+# create paths to locations in Drug-Discovery/$USER
+CONDA_PKGS=""$DRUG_DISCOVERY_PREFIX"/"$USER"/.conda_pkgs"
+CONDA_ENVS=""$DRUG_DISCOVERY_PREFIX"/"$USER"/.conda_envs"
+
+# Append package and environment directory locations # to the .condarc file in
+# the user's home directory. This is used by anaconda as the location to store
+# dependencies and what not. If this is not set
 cat <<EOT >> ~/.condarc
 pkgs_dirs:
     - $CONDA_PKGS
@@ -61,7 +70,10 @@ envs_dirs:
     - $CONDA_ENVS
 EOT
 
-
+# If the anaconda package and environment directories:
+#      /exports/eddie/Drug-Discovery/$USER/.conda_pkgs
+#      /exports/eddie/Drug-Discovery/$USER/.conda_envs
+# don't yet exist then these are created
 if [ ! -d "$CONDA_PKGS" ]
 then
     mkdir "$CONDA_PKGS"
@@ -72,7 +84,8 @@ then
     mkdir "$CONDA_ENVS"
 fi
 
-# create new directory to store the environment
+# Create new directory to store the environment. This environment is
+# unique to the user's CellProfiler installation.
 CONDA_ENV_DIRECTORY=""$DRUG_DISCOVERY_PREFIX"/$USER/"$ENV_NAME"_conda_env"
 
 if [ -d "$CONDA_ENV_DIRECTORY" ]
@@ -86,65 +99,75 @@ cd "$CONDA_ENV_DIRECTORY"
 
 
 
-# create an environment file and save it as 'environment.yml'
-# in the current directory
+# Create an environment file and save it as 'environment.yml' in the current
+# directory. This file contains the dependencies and versions required by
+# cellprofiler. Package version numbers are tricky and will need to be changed
+# if a different version of CellProfiler is installed. Dependencies unique to
+# CellProfiler (prokaryote, centrosome) are often updated so that they only
+# work with the latest version of CellProfiler.
 cat <<EOT >> environment.yml
-# to remove run: conda env remove -n $ENV_NAME
-name: $ENV_NAME
+# run: conda env create -f environment.yml
+# run: conda env update -f environment.yml
+# run: conda env remove -n cellprofiler
+name: cellprofiler
 # in order of priority: lowest (top) to highest (bottom)
 channels:
     - anaconda
-    - goodman
-    - bjornfjohansson # for wxpython on linux
+    - goodman # mysql-python for mac
+    - bjornfjohansson
     - bioconda
-    - cyclus # for java stuff
-    - conda-forge # for mahotas
+    - cyclus # java-jdk for windows
+    - conda-forge # libxml2 for windows
 dependencies:
     - appdirs
-    - boto3
-    - cython
-    - h5py
+    - cython=0.28.5
+    - h5py=2.8.0
     - ipywidgets
     - java-jdk
-    - jupyter
+    - jupyter=1.0.0
     - libtiff
     - libxml2
     - libxslt
-    - lxml
-    - packaging
-    - pillow
+    - lxml=4.2.5
+    - packaging=17.1
+    - pillow=5.2.0
     - pip
     - python=2
     - pyzmq=15.3.0
-    - mahotas
-    - matplotlib!=2.1.0,>2.0.0
+    - mahotas=1.4.4
+    - matplotlib=2.2.3
     - mysql-python
-    - numpy
-    - raven
-    - requests
-    - scikit-image>=0.13
-    - scikit-learn
-    - scipy
+    - numpy=1.14.5
+    - raven=6.3.0
+    - requests=2.18.4
+    - scikit-image=0.14.0
+    - scikit-learn=0.19.1
+    - scipy=1.1.0
     - sphinx
-    - tifffile
+    - tifffile=0.15.1
     - wxpython=3.0.2.0
     - pip:
-         - cellh5
-         - centrosome
-         - inflect
-         - prokaryote==2.3.3
-         - javabridge==1.0.15
-         - python-bioformats==1.4.0
-         - git+https://github.com/Swarchal/CellProfiler.git@master #CellProfiler fork frozen at version 3.0.0
+        - cellh5==1.3.0
+        - centrosome==1.0.9
+        - inflect==1.0.0
+        - prokaryote==2.4.0
+        - javabridge==1.0.15
+        - python-bioformats==1.4.0
+        - git+https://github.com/CarragherLab/CellProfiler.git@master
 EOT
 
-# fix JAVA_HOME so javabridge points to where anaconda's java install is going to be
+
+
+# Fix $JAVA_HOME so javabridge points to where anaconda's java install is going
+# to be.
 CONDA_ENV_PREFIX="$CONDA_ENVS"/"$ENV_NAME"
 export JAVA_HOME=$CONDA_ENV_PREFIX
 
-# call conda to create this environment from the environment.yml
+
+# Call conda to create this environment from the environment.yml
 conda env create -f environment.yml
-# update, as sometimes prokaryote and javabridge don't install properly
+# Call again and update, as sometimes prokaryote and javabridge don't install
+# properly
 conda env update -f environment.yml
 
 echo "DONE!"
